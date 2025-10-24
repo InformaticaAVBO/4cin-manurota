b89ed64fb97a99c7d3264a66c1464627808cdadd
manuel <132162356+manurota@users.noreply.github.com>
2025-10-24 07:40:22 +0000

    prova I/O

--- HelloWorld/src/App.java
+++ HelloWorld/src/App.java
@@ -1,5 +1,14 @@
+import java.io.FileWriter;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        FileWriter writer = new FileWriter("output.txt");
+        writer.write("CIAO questa è una prova");
+        writer.write("questa è la seconda riga");
+        writer.close();
+        
+    
+    
+    
     }
 }
